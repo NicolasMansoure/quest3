882de16a14d2995c5a8a93ddeefbec152ad793f0
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2023-01-30 20:20:11 +0100

    Update

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,7 +1,39 @@
-describe("Test d'api", () => {
-    it('Covid-19 API tests : General Stats', () => {
-        cy.visit('http://localhost:8080/app')
-        cy.request('users/1.json') //  URL is  http://localhost:8080/users/1.json
-    })
+/// <reference types="cypress" />
 
-})
+describe("Suite de tests sur une route similaire avec tous les paramètres", function() {
+    it("Lance une requête GET avec tous les paramètres nécessaires et recoit une réponse correspondant à ces paramètres", function() {
+        cy.fixture("data").then((requestData) => {
+            let { band, type, info, limit } = requestData.filter(
+                (item) => item.key === "allParams"
+            )[0];
+
+            cy.apiRequest(band, type, info, limit).then((response) => {
+                expect(response.status).to.be.eq(200);
+                expect(response.body).to.have.property("Similar");
+                expect(response.body.Similar.Info[0].Name.toLowerCase()).to.be.eq(
+                    band.toLowerCase()
+                );
+                expect(response.body.Similar.Results.length).to.be.eq(5);
+            });
+        });
+    });
+});
+
+describe("Suite de tests sur une route similaire sans aucun paramètre", function() {
+    it("Lance une requête avec des paramètres manquants et recoit une réponse de Body sans propriétés d'information et de résultat", function() {
+        cy.fixture("data.json").then((requestData) => {
+            let { band, type, info, limit } = requestData.filter(
+                (item) => item.key === "emptyParams"
+            )[0];
+
+            cy.apiRequest(band, type, info, limit).then((response) => {
+                expect(response.status).to.be.eq(200);
+                expect(response.body).to.have.property("Similar");
+                expect(response.body.Similar.Info.length).to.be.eq(2);
+                expect(response.body.Similar.Info[0].Name).to.be.eq("");
+                expect(response.body.Similar.Info[1].Name).to.be.eq("");
+                expect(response.body.Similar.Results.length).to.be.eq(0);
+            });
+        });
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+///<reference types="cypress"/>
+
+Cypress.Commands.add("apiRequest", (query, type, info, limit) => {
+    cy.request({
+        url: "https://tastedive.com/api/similar",
+        qs: {
+            q: query,
+            type: type,
+            info: info,
+            limit: limit,
+        },
+    });
+});
